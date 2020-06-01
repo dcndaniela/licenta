47367863c4f23b4bd0ca42f99b4a3f582e2b4bd1
dcndaniela <diaconudaniela@yahoo.com>
2020-06-01 22:21:57 +0300

    Commit7: + PostgreSQL database

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# from django.contrib.auth.models import(
+#         AbstractBaseUser
+#     )
+#
+# class User(AbstractBaseUser):  #clasa User pe care o scriu eu, ca sa inlocuiasca User django class
+#     email=
--- mysite/settings.py
+++ mysite/settings.py
@@ -68,11 +68,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite', # numele BD create in pgAdmin
+        'USER': 'postgres', # Adminul
+        'PASSWORD': 'Administrator1!',  #password pentru postgres (postgre supersuser)
+        'HOST': '127.0.0.1',
+        'PORT': '5432', # port default pentru PostgreSQL
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
